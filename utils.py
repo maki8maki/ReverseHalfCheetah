a518655146bb8c0c87be1e25c3eb7274a3b490a7
yuki_y <chrisrerei125815@gmail.com>
2024-03-19 22:47:08 +0900

    seed固定関数の実装

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import gymnasium as gym
+import numpy as np
+import random
+import torch as th
 
 from wrapper import ImageObservationWrapper, RepeatActionWrapper
 
@@ -6,3 +9,13 @@ def make_env(env_name: str, config: dict = {}):
     env = ImageObservationWrapper(gym.make(env_name, render_mode='rgb_array', **config))
     env = RepeatActionWrapper(env, skip=2)
     return env
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    th.manual_seed(seed)
+    th.backends.cudnn.deterministic = True
+    th.backends.cudnn.benchmark = False
+    if th.cuda.is_available():
+        th.cuda.manual_seed(seed)
+        th.cuda.manual_seed_all(seed)
